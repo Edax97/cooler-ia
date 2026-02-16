547ed3ec0cd19d62f391f24f8abeccc8f00810e0
Edwin Campos <ecamposc97@gmail.com>
2026-02-16 18:23:07 -0500

    v0.2: module imports

--- cooler.py
+++ cooler.py
@@ -1,4 +1,5 @@
 from app.app import detect_forbidden
 
 if __name__ == "__main__":
-    detect_forbidden(0, is_raspberry=True)
+
+    detect_forbidden("app/videos/botellas2.mp4", is_raspberry=False)
